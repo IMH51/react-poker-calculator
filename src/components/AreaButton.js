f1c724c67caef0c87129270cf38cf28aff60645d
IMH51 <ianmartinharrison@outlook.com>
2019-11-16 19:11:25 +0000

    destructured, added name helper, ternary refactor

--- src/components/AreaButton.js
+++ src/components/AreaButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { getAreaName } from "../helpers/areaData"
 
-const AreaButton = props => {
-    const buttonText = props.selected === props.area ? `Adding to ${props.area} Cards` : `${props.area} Cards`
-    const buttonClass = props.selected === props.area ? "selected-button" : ""
+const AreaButton = ({ selectedArea, areaName, disable, handleClick}) => {
+    const buttonText = `${selectedArea === areaName ? "Adding to" : ""} ${getAreaName(areaName)} Cards`
+    const buttonClass = selectedArea === areaName ? "selected-button" : "" 
     return (
-      <button className={buttonClass} disabled={props.disable} onClick={props.handleClick}>{buttonText}</button>
+      <button className={buttonClass} disabled={disable} onClick={handleClick}>{buttonText}</button>
     )
 }
 
