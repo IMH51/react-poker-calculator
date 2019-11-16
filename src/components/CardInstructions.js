fb7da5da0df804d055465bdedd5ece3b13c3a6b9
IMH51 <ianmartinharrison@outlook.com>
2019-11-16 19:09:57 +0000

    destructured and added in name helper

--- src/components/CardInstructions.js
+++ src/components/CardInstructions.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { getAreaName } from "../helpers/areaData"
 
-const CardInstructions = (props) => {
+const CardInstructions = ({ selectedArea }) => {
+  const name = getAreaName(selectedArea)
   return (
   <>
-    <p>Click on a card above to add it to the {props.selected} area. To add one to a different area, select it using the buttons below.</p>
+    <p>Click on a card above to add it to the {name} area. To add one to a different area, select it using the buttons below.</p>
     <p></p>
     <p>To remove a card from an area, simply click on it!</p>
   </>
