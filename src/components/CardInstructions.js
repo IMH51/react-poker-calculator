7edac4502f37af31d04b098ee90dd2abaac0136b
IMH51 <ianmartinharrison@outlook.com>
2019-11-19 12:25:40 +0000

    used clearer variables, removed area name function

--- src/components/CardInstructions.js
+++ src/components/CardInstructions.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { getAreaString } from "../helpers/areaData"
 
-const CardInstructions = ({ selectedArea }) => {
-  const name = getAreaString(selectedArea)
+const CardInstructions = ({ areaName }) => {
   return (
   <>
-    <p>Click on a card above to add it to the {name} area. To add one to a different area, select it using the buttons below.</p>
+    <p>Click on a card above to add it to the {areaName} area. To add one to a different area, select it using the buttons below.</p>
     <p></p>
     <p>To remove a card from an area, simply click on it!</p>
   </>
