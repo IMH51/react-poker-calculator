66c16e3bb64ad4ab5992ebb74f1e417ef57552d2
IMH51 <ianmartinharrison@outlook.com>
2019-11-19 12:26:05 +0000

    added clearer variable names, refactored

--- src/containers/TableContainer.js
+++ src/containers/TableContainer.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Card from "../components/Card"
 import AreaButton from "../components/AreaButton"
 
-const TableContainer = ({ cards, selectedArea, areaName, disable, setSelected, removeCard }) => {
-  const selectedClass = selectedArea === areaName && "selected-area"
-  const handleClick = () => setSelected(areaName)
+const TableContainer = ({ cards, selectedKey, areaKey, areaName, disable, setSelectedKey, removeCard }) => {
+  const selectedClass = selectedKey === areaKey && "selected-area"
+  const handleClick = () => setSelectedKey(areaKey)
   return (
     <div className={`table-card-containers ${selectedClass}`}>
-      <AreaButton {...{selectedArea, areaName, disable, handleClick}}/>
+      <AreaButton {...{selectedKey, areaKey, areaName, disable, handleClick}}/>
       <div >
-        {cards.map(card => <Card key={card.name} clickHandler={() => removeCard(card, areaName)} {...{areaName, disable, card}} />)}
+        {cards.map(card => {
+        return <Card key={card.name} clickHandler={() => removeCard(card, areaKey)} {...{areaName, disable, card}} />
+        })}
       </div>
     </div>
   )
