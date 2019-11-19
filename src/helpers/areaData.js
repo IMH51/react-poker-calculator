e3e28452f1ca16f78c48e604664a7fe3dc9d218d
IMH51 <ianmartinharrison@outlook.com>
2019-11-19 12:26:23 +0000

    removed area string method, renamed keys

--- src/helpers/areaData.js
+++ src/helpers/areaData.js
@@ -1,11 +1,9 @@
 const areas = {
-    player1: {name: "Player 1", cards: [], limit: 2},
-    table: {name: "Table", cards: [], limit: 3},
-    player2: {name: "Player 2", cards: [], limit: 2}
+    player1: {areaName: "Player 1", cards: [], limit: 2},
+    table: {areaName: "Table", cards: [], limit: 3},
+    player2: {areaName: "Player 2", cards: [], limit: 2}
   }
   
 const areaKeys = Object.keys(areas)
 
-const getAreaString = name => `${name[0].toUpperCase()}${name.slice(1,6)} ${name[6] || ""}`
-
-export { areas, areaKeys, getAreaString }
+export { areas, areaKeys }
