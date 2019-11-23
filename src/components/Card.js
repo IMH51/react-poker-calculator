7c0392ee14e9178ec95a34bb583042a970e1927d
IMH51 <ianmartinharrison@outlook.com>
2019-11-23 15:19:10 +0000

    tweaks for github pages deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({ card: { name, code}, disable, clickHandler}) => {
   const clickFunction = disable ? undefined : clickHandler
   return (
-  <img className="small-card" src={`/imgs/cards/${code}.png`} alt={name} onClick={clickFunction} />
+  <img className="small-card" src={`imgs/cards/${code}.png`} alt={name} onClick={clickFunction} />
   )
 }
 
