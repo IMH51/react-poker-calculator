a84cae9d319d9fc21bd76c2e7199b0d940cd7178
IMH51 <ianmartinharrison@outlook.com>
2019-11-23 15:06:49 +0000

    Updates

---
+++ precache-manifest.797ee8bf67e5cc7c3f767f33cbcb14e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aee4cdfa8b5f6c0e7d07868d31c58d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1a8def24491c54cf74f9",
+    "url": "/my-app/static/css/main.d8ad1a0c.chunk.css"
+  },
+  {
+    "revision": "c00338d5be220f4b4e2c",
+    "url": "/my-app/static/js/2.4cd01eb8.chunk.js"
+  },
+  {
+    "revision": "1a8def24491c54cf74f9",
+    "url": "/my-app/static/js/main.743701ad.chunk.js"
+  },
+  {
+    "revision": "fe709be1d223c4bcec7d",
+    "url": "/my-app/static/js/runtime-main.fd21a4c4.js"
+  }
+]);
